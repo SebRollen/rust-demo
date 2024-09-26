2517b9b04b8baf344ad0cf3a26b875ff9f2d7001
Sebastian Rollen <seb@rollen.io>
2024-09-26 12:16:53 -0400

    checkpoint6

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,17 @@ use chrono::{Datelike, NaiveDateTime};
 struct Subscription {
     plan: String,
     status: Status,
-    // New field! when did a user cancel?
-    // `Option` is a built-in enum to reflect that a type may not exist.
-    // It's definition is basically:
-    // enum Option<T> {
-    //   Some(T),
-    //   None
-    // }
-    cancelled_at: Option<NaiveDateTime>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 enum Status {
     Active,
-    Cancelled,
+    Cancelled {
+        // Surprise! enums can also contain state.
+        // In this case, we don't use an Option<NaiveDateTime>, because we want to guarantee that a
+        // subscription in the Cancelled state has a cancelled_at time set.
+        cancelled_at: NaiveDateTime,
+    },
     Paused,
 }
 
@@ -27,7 +24,6 @@ impl Subscription {
         Self {
             plan: plan.into(),
             status: Status::Active,
-            cancelled_at: None,
         }
     }
 
@@ -37,36 +33,39 @@ impl Subscription {
 
     fn is_inactive(&self) -> bool {
         match self.status {
-            Status::Cancelled | Status::Paused => true,
+            Status::Cancelled { .. } | Status::Paused => true,
             Status::Active => false,
         }
     }
 
     fn cancelled_at_month(&self) -> Option<u32> {
-        // We can not call self.cancelled_at.month() like we do in ruby, because cancelled_at might
-        // be None. Rust forces us to check for nullity, which makes it impossible to get errors
-        // like `undefined method `id' for nil:NilClass`
-        //
-        // The way we've written it below is the "dumbest" way to write it. Can also do something
-        // like
-        // self.cancelled_at.map(|d| d.month())
-        // or
-        // let d = self.cancelled_at?;
-        // Some(d.month())
-        match self.cancelled_at {
-            Some(d) => Some(d.month()),
-            None => None,
+        // We now need to match on status rather than cancelled_at, since the field lives inside
+        // the status enum
+        match self.status {
+            Status::Cancelled { cancelled_at } => Some(cancelled_at.month()),
+            // Any other status us uncancelled, so all we can do is return None
+            _ => None,
         }
+    }
+
+    // This method takes &mut self, and returns nothing, meaning that we just modify our instance
+    // in place.
+    // We'll return the cancellation date for easy inspection
+    fn cancel(&mut self) -> NaiveDateTime {
+        let cancelled_at = chrono::Local::now().naive_local();
+        self.status = Status::Cancelled { cancelled_at };
+        cancelled_at
     }
 }
 
 fn main() {
     let mut sub = Subscription::new("my-plan");
-    dbg!(&sub.status);
+    // This is no longer valid code. We've made it impossible to get into an inconsistent state!
+    // sub.status = Status::Cancelled;
+    dbg!(sub.cancel());
     dbg!(&sub.cancelled_at_month());
-    sub.status = Status::Cancelled;
-    // Whoops, this is an inconsistent state… We want to make it impossible to have a `Cancelled`
-    // status with a `None` cancelled_at.
-    dbg!(&sub.status);
+    // Question: what happens if we cancel twice?
+    dbg!(sub.cancel());
+    // Uh-oh…
     dbg!(&sub.cancelled_at_month());
 }
