fd557bce3918b4c20277c7a165dfdc81308464da
Sebastian Rollen <seb@rollen.io>
2024-09-26 11:34:13 -0400

    checkpoint1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#![allow(dead_code)]
+
+// Our data. We want to model a subscription that's subscribed to a particular plan, and has some
+// status.
+#[derive(Debug)]
+struct Subscription {
+    plan: String,
+    status: String,
+}
+
+// In rust, the data definition and its methods are separated. The `impl` block lets us add methods
+// to the struct.
+impl Subscription {
+    // Convenience function for creating a new Subscription. We could also build one directly:
+    // let sub = Subscription { plan: "my-plan".to_string(), status: "active".to_string }
+    //
+    // One benefit of the `new` method is that we can a default value for `status`
+    fn new(plan: impl Into<String>) -> Self {
+        Self {
+            plan: plan.into(),
+            status: "active".to_string(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sub = Subscription::new("my-plan");
+    // dbg! is a macro that lets you inspect various objects
+    dbg!(sub);
 }
